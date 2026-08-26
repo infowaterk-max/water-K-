a90891fdb6bddf3a8331591cba4c31659a0fc54a
infowaterk-max <info.waterk@gmail.com>
2026-08-26 18:01:26 +0200

    Type shipping provider stubs against integration contract

--- src/lib/integrations/shipping.ts
+++ src/lib/integrations/shipping.ts
@@ -1,4 +1,30 @@
 import type { ShippingProvider } from './types';
-export class FoxpostShipping implements ShippingProvider{async quote(){throw new Error('Foxpost API credentials required');}async createShipment(){throw new Error('Foxpost API credentials required');}}
-export class GlsShipping implements ShippingProvider{async quote(){throw new Error('GLS API credentials required');}async createShipment(){throw new Error('GLS API credentials required');}}
-export class MplShipping implements ShippingProvider{async quote(){throw new Error('MPL API credentials required');}async createShipment(){throw new Error('MPL API credentials required');}}
+
+type QuoteInput = Parameters<ShippingProvider['quote']>[0];
+type ShipmentInput = Parameters<ShippingProvider['createShipment']>[0];
+type QuoteResult = ReturnType<ShippingProvider['quote']>;
+type ShipmentResult = ReturnType<ShippingProvider['createShipment']>;
+
+abstract class UnconfiguredShippingProvider implements ShippingProvider {
+  protected abstract readonly providerName: string;
+
+  async quote(_input: QuoteInput): QuoteResult {
+    throw new Error(`${this.providerName} API credentials required`);
+  }
+
+  async createShipment(_input: ShipmentInput): ShipmentResult {
+    throw new Error(`${this.providerName} API credentials required`);
+  }
+}
+
+export class FoxpostShipping extends UnconfiguredShippingProvider {
+  protected readonly providerName = 'Foxpost';
+}
+
+export class GlsShipping extends UnconfiguredShippingProvider {
+  protected readonly providerName = 'GLS';
+}
+
+export class MplShipping extends UnconfiguredShippingProvider {
+  protected readonly providerName = 'MPL';
+}
