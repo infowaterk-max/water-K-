7dc3b55424078d4cb200237548a25ac69ec7a37a
infowaterk-max <info.waterk@gmail.com>
2026-08-26 16:43:18 +0200

    Create Water-K customizations plugin

---
+++ wp-content/plugins/waterk-customizations/waterk-customizations.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Water-K Customizations
+ * Description: A Water-K webáruház saját WooCommerce és üzleti funkciói.
+ * Version: 1.0.0
+ * Author: Water-K
+ * Text Domain: waterk
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'WATERK_CUSTOMIZATIONS_VERSION' ) ) {
+    define( 'WATERK_CUSTOMIZATIONS_VERSION', '1.0.0' );
+}
+
+/**
+ * A funkciókat külön modulokban tartjuk, hogy a webshop fejlesztése
+ * átlátható és biztonságosan karbantartható maradjon.
+ */
+$waterk_modules = array(
+    'woocommerce.php',
+    'customer-roles.php',
+    'notifications.php',
+);
+
+foreach ( $waterk_modules as $waterk_module ) {
+    $waterk_module_path = __DIR__ . '/includes/' . $waterk_module;
+
+    if ( file_exists( $waterk_module_path ) ) {
+        require_once $waterk_module_path;
+    }
+}
