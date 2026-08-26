6718b93fd55cfc06c603f1499ccfda8a750c851c
infowaterk-max <info.waterk@gmail.com>
2026-08-26 18:15:19 +0200

    Remove obsolete WordPress customer role module

--- wp-content/plugins/waterk-customizations/includes/customer-roles.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Water-K lakossági / viszonteladói jogosultságok és láthatóság.
- */
-
-defined( 'ABSPATH' ) || exit;
-
-// Ide kerülnek a B2C/B2B szerepkör-, kategória- és termékláthatósági szabályok.
