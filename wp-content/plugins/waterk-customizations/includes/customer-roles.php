99d6277703bab4d9aeca2e79c45c17c35bbf920c
infowaterk-max <info.waterk@gmail.com>
2026-08-26 16:43:30 +0200

    Add customer role customization module

---
+++ wp-content/plugins/waterk-customizations/includes/customer-roles.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Water-K lakossági / viszonteladói jogosultságok és láthatóság.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Ide kerülnek a B2C/B2B szerepkör-, kategória- és termékláthatósági szabályok.
