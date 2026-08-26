41534a65a9f198f4321ec437c72a859ca2fccc21
infowaterk-max <info.waterk@gmail.com>
2026-08-26 17:16:32 +0200

    Switch homepage to live WooCommerce product data

--- wp-content/plugins/waterk-customizations/includes/homepage.php
+++ wp-content/plugins/waterk-customizations/includes/homepage.php
@@ -5,15 +5,11 @@
 
 defined( 'ABSPATH' ) || exit;
 
-/**
- * Compose the complete first-generation Water-K landing page.
- * Usage in a WordPress page / UX Builder shortcode element: [waterk_homepage]
- */
 function waterk_shortcode_homepage() {
     $sections = array(
         '[waterk_hero]',
         '[waterk_benefits]',
-        '[waterk_product_selector]',
+        '[waterk_live_products]',
         '[waterk_process]',
         '[waterk_faq]',
     );
