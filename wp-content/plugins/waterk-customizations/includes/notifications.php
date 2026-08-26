ed35026667137db49076d65edccfa3d3693b1e28
infowaterk-max <info.waterk@gmail.com>
2026-08-26 16:43:34 +0200

    Add notification customization module

---
+++ wp-content/plugins/waterk-customizations/includes/notifications.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Water-K WooCommerce értesítések és e-mail szabályok.
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Ide kerülnek az egyedi rendelési, logisztikai és ügyfélértesítési szabályok.
