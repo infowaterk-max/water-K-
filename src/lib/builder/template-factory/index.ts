6e9d6eba58da65227fca6ec4cde8b9f3ff854f06
infowaterk-max <info.waterk@gmail.com>
2026-09-23 13:30:36 +0200

    feat: expose one-call Template Factory candidate build

---
+++ src/lib/builder/template-factory/index.ts
@@ -0,0 +1,16 @@
+import {getStorefrontTemplateFactoryCategoryFoundation} from '@/lib/builder/template-factory/category-foundations';
+import {
+  compileStorefrontTemplateFactoryPackage,
+  type StorefrontTemplateFactoryBuild,
+  type StorefrontTemplateFactoryRecipe,
+} from '@/lib/builder/template-factory/scaffold';
+
+export function buildStorefrontTemplateFactoryCandidate(recipe:StorefrontTemplateFactoryRecipe):StorefrontTemplateFactoryBuild{
+  return compileStorefrontTemplateFactoryPackage({
+    foundation:getStorefrontTemplateFactoryCategoryFoundation(recipe.category),
+    recipe,
+  });
+}
+
+export * from '@/lib/builder/template-factory/scaffold';
+export * from '@/lib/builder/template-factory/category-foundations';
