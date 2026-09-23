f669354169df0df5c6ca9ae111e1fed5e350bb87
infowaterk-max <info.waterk@gmail.com>
2026-09-23 13:32:42 +0200

    feat: declare Gaming foundation leak tokens

--- src/lib/builder/template-factory/category-foundations.ts
+++ src/lib/builder/template-factory/category-foundations.ts
@@ -9,6 +9,7 @@ export const GAMING_TEMPLATE_FACTORY_FOUNDATION:StorefrontTemplateFactoryCategor
   package:PLAYROOM_V20_TEMPLATE_PACKAGE,
   recommendedOwnedPages:Object.freeze(['home','catalog','product','blog-index','blog-article']),
   inheritedPages:Object.freeze(STOREFRONT_PAGE_TYPES.filter(page=>!['home','catalog','product','blog-index','blog-article'].includes(page))),
+  forbiddenLeakTokens:Object.freeze(['PLAYROOM','Playroom','/storefront/playroom/']),
 });
 
 export const STOREFRONT_TEMPLATE_FACTORY_CATEGORY_FOUNDATIONS=Object.freeze({
